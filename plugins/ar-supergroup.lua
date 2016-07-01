a09642dcb47de25dc75fa69077face219e77a3fc
zaeem998 <alm3lm.ms@gmail.com>
2016-07-01 05:09:13 +0300

    Update ar-supergroup.lua

--- plugins/ar-supergroup.lua
+++ plugins/ar-supergroup.lua
@@ -10,6 +10,7 @@
 #:))
 #ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
 #
+]]
 local function check_member_super(cb_extra, success, result)
   local receiver = cb_extra.receiver
   local data = cb_extra.data
