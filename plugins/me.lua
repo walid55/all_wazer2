1b881ddf3ee9d5e22bf26405020c889023360db2
محمد <alm3lm.ms@gmail.com>
2016-07-22 09:40:59 +0000

    Add files via upload

---
+++ plugins/me.lua
@@ -0,0 +1,39 @@
+--[[
+# @m1_l1
+# BY : vtbot
+]]
+do
+
+local function oscar(msg ,matches)
+        if is_sudo(msg) then
+local url = "http://e.top4top.net/p_2016mqh2.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+  send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+
+        elseif is_owner(msg) then
+        local url = "http://d.top4top.net/p_2010lto1.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+
+      send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+        elseif is_momod(msg) then
+        local url = "http://c.top4top.net/p_201090j0.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+
+   send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+        elseif not is_momod(msg) then
+        local url = "http://a.top4top.net/p_201lshk3.jpg"
+local mustafadev = download_to_file(url,'DeaD.jpg')
+
+     send_photo(get_receiver(msg),mustafadev,ok_cb,false)
+        end
+end
+
+return { 
+    patterns = { 
+        "^(me)$"
+    },
+    run = oscar,
+    
+}
+--by @m1_l1
+end
