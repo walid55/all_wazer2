6942854124c12756e6c23c51edcdc4c3bb65cd1a
محمد <alm3lm.ms@gmail.com>
2016-08-12 06:57:26 +0000

    Update vrbot.lua

--- plugins/vrbot.lua
+++ plugins/vrbot.lua
@@ -6,7 +6,7 @@ if ( msg.text ) then
 
   if ( msg.to.type == "user" ) then
 
-     return "للتحدث مع المطور اضغط على المعرف التالي Developer :  < @M1_l1 > Developer :  < @A1_b1 >"
+     return "للتحدث مع المطور اضغط على المعرف التالي Develoer  < @DEV_m1 > Developer :  < @A1_b1 >"
      
   end 
    
